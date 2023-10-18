6cf22e5c75e91ff5051c9abe1566a1b0c5dd7a9f
somya agrawal <127408693+Somya-Agrawal1010@users.noreply.github.com>
2023-10-18 23:54:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "your_username" && password === "your_password") {
+            alert("Login successful");
+            // You can redirect the user to another page here
+        } else {
+            alert("Invalid username or password");
+        }
+    });
+});
